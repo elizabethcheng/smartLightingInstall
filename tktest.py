e231927cff5e249996f65da88b5a335798c9e4ed
elizabethcheng <elizabeth.y.cheng@gmail.com>
2013-12-13 12:13:04 -0800

    Fixed loading bar, window height size, disable buttons, element lagging problem

--- tktest.py
+++ tktest.py
@@ -1,7 +1,9 @@
 import Tkinter as tk
 import tkFont
-import time
 import os
+import ttk
+import time
+import threading
 
 from time import mktime, localtime, gmtime, strftime
 from Tkinter import *
@@ -73,18 +75,13 @@ class Application(tk.Frame):
         self.columnconfigure(1, minsize="120")
         self.columnconfigure(2, minsize="120")
         self.rowconfigure(0, minsize="120", pad="20")
-        self.rowconfigure(1, minsize="50")
-        self.rowconfigure(2, minsize="60")
+        self.rowconfigure(1, minsize="100")
+        self.rowconfigure(2, minsize="100")
         self.rowconfigure(3, minsize="150", pad="20")
         # Create application window
         self.grid()
         # Create widgets for first page
         self.titlePage()
-    
-    def make_unix_timestamp(date_string, time_string):
-        """This command converts string format of date into unix timstamps."""
-        format = '%Y %m %d %H %M %S'
-        return time.mktime(time.strptime(date_string + " " + time_string, format))
 
     def demo(self, meter, value):
         meter.set(value)
@@ -117,67 +114,102 @@ class Application(tk.Frame):
         self.nextButton = tk.Button(self, text = 'Next', command = self.next)
         self.nextButton.grid(column=1, row=3)
         self.quitButton.grid(column=2, row=3)
-        # Increment page count (needed in self.next method)
+        # Create the button but DO NOT GRID IT
+        self.finishButton = tk.Button(self, text = 'Finish', command = self.lastSensor)
 
     def tinyOSInstallPage(self):
         #self.image.grid_forget()
         self.nextButton.config(state='disabled')
         self.text['text'] = "Please wait while the wizard installs TinyOS software."
         self.text.grid(row=1, column=0, columnspan=3, rowspan=2)
-        self.m = Meter(self, relief='ridge', bd=3)
-        self.m.grid(row=2, column=0, columnspan=3)
-        self.m.set(0.0, 'Starting demo...')
-        self.m.after(10, lambda: self.demo(self.m, 0.0))
-        # Call backend function here!
-        #self.installTinyOS()
-        self.nextButton.config(state='normal')
+        #self.m = Meter(self, relief='ridge', bd=3)
+        #self.m.grid(row=2, column=0, columnspan=3)
+        #self.m.set(0.0, 'Starting demo...')
+        #self.m.after(10, lambda: self.demo(self.m, 0.0))
+        self.pb = ttk.Progressbar(self, orient="horizontal", length = 200, mode="indeterminate")
+        self.pb.grid(row=2, column=0, columnspan=3)
+        global install_thread
+        install_thread = threading.Thread(target=self.installTinyOS)
+        install_thread.daemon = True
+        self.pb.start()
+        install_thread.start()
+        self.after(20, self.check_tinyos_thread)
+
+    def check_tinyos_thread(self):
+        if install_thread.is_alive():
+            self.after(20, self.check_tinyos_thread)
+        else:
+            self.pb.stop()
+            self.text['text'] = "TinyOS software successfully installed."
+            self.text.grid(rowspan=2)
+            self.nextButton.config(state='normal')
 
     def smapFolderPage(self):
-        self.m.grid_forget()
+        self.pb.grid_forget()
         self.text['text'] = "Enter a name for your sMAP data folder:"
-        self.text.grid(column=0, row=1, columnspan=3, rowspan=2)
         self.entry = tk.Entry(self, cursor="xterm", exportselection=0, \
             textvariable=self.entryText)
         self.entry.grid(column=0, row=2, columnspan=3)
+        self.update_idletasks()
 
     def smapInstallPage(self):
         self.nextButton.config(state='disabled')
         self.entry.grid_forget()
         self.text['text'] = "Please wait while the wizard sets up sMAP."
         self.text.grid(row=1, column=0, columnspan=3, rowspan=2)
-        self.m = Meter(self, relief='ridge', bd=3)
-        self.m.grid(row=2, column=0, columnspan=3)
-        self.m.set(0.0, 'Starting demo...')
-        self.m.after(10, lambda: self.demo(self.m, 0.0))
-        # Call backend function here!
-        #self.installSmap()
-        #If successful, change text to: "sMAP successfully installed."
-        self.text['text'] = "sMAP successfully installed."
-        self.nextButton.config(state='normal')
+        #self.m = Meter(self, relief='ridge', bd=3)
+        #self.m.grid(row=2, column=0, columnspan=3)
+        #self.m.set(0.0, 'Starting demo...')
+        #self.m.after(10, lambda: self.demo(self.m, 0.0))
+        self.pb = ttk.Progressbar(self, orient="horizontal", length = 200, mode="indeterminate")
+        self.pb.grid(row=2, column=0, columnspan=3)
+        global install_thread
+        install_thread = threading.Thread(target=self.installSmap)
+        install_thread.daemon = True
+        self.pb.start()
+        install_thread.start()
+        self.after(20, self.check_smap_thread)
+
+    def check_smap_thread(self):
+        if install_thread.is_alive():
+            self.after(20, self.check_smap_thread)
+        else:
+            self.pb.stop()
+            self.text['text'] = "sMAP software successfully installed."
+            self.text.grid(rowspan=2)
+            self.nextButton.config(state='normal')
 
     def sensorPlugPage(self):
+        self.pb.grid_forget()
         self.finishButton.grid_forget()
-        self.m.grid_forget()
         self.columnconfigure(0, minsize="350")
         self.columnconfigure(1, minsize="120")
         self.columnconfigure(2, minsize="120")
+        self.columnconfigure(3, minsize="0")
+        self.nextButton.grid(row=3, column=1)
+        self.quitButton.grid(row=3, column=2)
         self.text['text'] = "Pick a light sensor and put two batteries in it. Plug the light sensor into a USB port. Click 'Next' when you are finished."
         self.text.grid(row=1, column=0, columnspan=3, rowspan=2)
+        self.title.grid(row=0, column=0, columnspan=3)
+        self.update_idletasks()
 
     def sensorIDPage(self):
         self.entryText.set('')
         self.text['text'] = "Please input the sensor ID of the sensor plugged into " + \
                 "your computer. (Ex: '1', '4', etc.)."
-        self.entry.grid(column=0, row=2, columnspan=3)
+        #self.text.grid(rowspan=1)
+        self.entry.grid(column=0, row=2, columnspan=3, rowspan=1)
+        self.update_idletasks()
 
     def sensorConfigurePage(self):
+        self.entry.grid_forget()
         self.columnconfigure(0, minsize="230")
         self.columnconfigure(1, minsize="120")
         self.columnconfigure(2, minsize="120")
         self.columnconfigure(3, minsize="120")
-        self.entry.grid_forget()
+        self.title.grid(row=0, column=0, columnspan=4)
         self.text['text'] = "Please wait while we configure the light sensor." 
-        self.text.grid(row=1, column=0, columnspan=4, rowspan=1)
+        self.text.grid(row=1, column=0, rowspan=2, columnspan=4)
         # Create the 'Last Sensor' button, which indicates that user is finished inputting light sensors
         self.nextButton.grid(row=3, column=1)
         self.quitButton.grid(row=3, column=3)
@@ -185,40 +217,84 @@ class Application(tk.Frame):
         self.finishButton.grid(row=3, column=2)
         self.nextButton.config(state='disabled')
         self.finishButton.config(state='disabled')
-        self.m = Meter(self, relief='ridge', bd=3)
-        self.m.grid(row=2, column=0, columnspan=4)
-        self.m.set(0.0, 'Starting demo...')
-        self.m.after(10, lambda: self.demo(self.m, 0.0))
+        #self.m = Meter(self, relief='ridge', bd=3)
+        #self.m.grid(row=2, column=0, columnspan=4)
+        #self.m.set(0.0, 'Starting demo...')
+        #self.m.after(10, lambda: self.demo(self.m, 0.0))
+        self.pb = ttk.Progressbar(self, orient="horizontal", length = 200, mode="indeterminate")
+        self.pb.grid(row=2, column=0, columnspan=4, rowspan=1)
+        global install_thread
+        install_thread = threading.Thread(target=self.configureSensor)
+        install_thread.daemon = True
+        self.pb.start()
+        install_thread.start()
+        self.after(20, self.check_sensorconfig_thread)
         #self.configureSensor()
         #If successful, change text to: "Light sensor successfully installed"
-        self.text['text'] = "Light sensor 4 successfully installed"
-        self.nextButton.config(state='normal')
-        self.finishButton.config(state='normal')
+        self.update_idletasks()
+
+    def check_sensorconfig_thread(self):
+        if install_thread.is_alive():
+            self.after(20, self.check_sensorconfig_thread)
+        else:
+            self.pb.stop()
+            self.text['text'] = "Light sensor " + self.entryText.get() + " successfully installed"
+            #self.text.grid(rowspan=2)
+            self.nextButton.config(state='normal')
+            self.finishButton.config(state='normal')
 
     def baseStationPlugPage(self):
-        self.text['text'] = "Put two batteries in the BaseStation mote."
-        self.text2 = tk.Label(self, text = "Plug the BaseStation mote into a USB port." +\
-            " Click 'Next' when you are finished.", wraplength = "450", justify = LEFT, \
-            anchor = W, font=self.textFont) 
-        self.text2.grid(column=0, row=3, columnspan=3)
+        self.pb.grid_forget()
+        self.finishButton.grid_forget()
+        self.quitButton['text'] = "Cancel"
+        self.columnconfigure(0, minsize="350")
+        self.columnconfigure(1, minsize="120")
+        self.columnconfigure(2, minsize="120")
+        self.columnconfigure(3, minsize="0")
+        self.text['text'] = "Put two batteries in the BaseStation mote. Plug the " +\
+            "BaseStation mote into a USB port. Click 'Next' when you are finished."
+        self.text.grid(row=1, column=0, columnspan=3, rowspan=2)
+        self.title.grid(row=0, column=0, columnspan=3)
+        self.nextButton.grid(row=3, column=1)
+        self.quitButton.grid(row=3, column=2)
+        self.update_idletasks()
         
     def baseStationConfigurePage(self):
-        self.text2.grid_forget()
+        self.nextButton.config(state='disabled')
+        self.quitButton.config(state='disabled')
         self.text['text'] = "Please wait while we configure the BaseStation." 
-        self.m = Meter(self, relief='ridge', bd=3)
-        self.m.grid(row=2, column=0, columnspan=3)
-        self.m.set(0.0, 'Starting demo...')
-        self.m.after(10, lambda: self.demo(self.m, 0.0))
-        #self.configureBaseStation()
-        #If successful, change text to: "Light sensor successfully installed"
-        self.text['text'] = "BaseStation successfully installed."
+        #self.text.grid(row=1, column=0, columnspan=3, rowspan=1)
+        #self.m = Meter(self, relief='ridge', bd=3)
+        #self.m.grid(row=2, column=0, columnspan=3)
+        #self.m.set(0.0, 'Starting demo...')
+        #self.m.after(10, lambda: self.demo(self.m, 0.0))
+        self.pb = ttk.Progressbar(self, orient="horizontal", length = 200, mode="indeterminate")
+        self.pb.grid(row=2, column=0, columnspan=3)
+        global install_thread
+        install_thread = threading.Thread(target=self.configureBaseStation)
+        install_thread.daemon = True
+        self.pb.start()
+        install_thread.start()
+        self.after(20, self.check_baseconfig_thread)
+
+    def check_baseconfig_thread(self):
+        if install_thread.is_alive():
+            self.after(20, self.check_baseconfig_thread)
+        else:
+            self.pb.stop()
+            self.text['text'] = "BaseStation successfully installed."
+            self.text.grid(rowspan=2)
+            self.nextButton.config(state='normal')
+            self.quitButton.config(state='normal')
 
     def congratzPage(self):
-        self.m.grid_forget()
+        self.pb.grid_forget()
+        self.nextButton.grid_forget()
         self.text['text'] = "Congratulations! You have successfully installed " + \
             "SmartLighting. Click 'Finish' to exit the installation wizard."
+        self.text.grid(row=1, column=0, columnspan=3, rowspan=2)
         self.quitButton['text'] = "Finish"
-        #If successful, change text to: "Light sensor successfully installed"
+        self.update_idletasks()
 
     def next(self):
         # Set the correct page number
@@ -246,34 +322,54 @@ class Application(tk.Frame):
         elif self.page == 7:
             self.sensorConfigurePage()
         elif self.page == 8:
+            self.baseStationPlugPage()
+        elif self.page == 9:
+            self.baseStationConfigurePage()
+        elif self.page == 10:
             self.congratzPage()
 
     def installTinyOS(self):
         #run terminal command here
-        call(["sh", "tinyos_install.sh"])
+        #call(["sh", "tinyos_install.sh"])
+        x = [i for i in range(2)]
+        for elem in x:
+            print x
+            time.sleep(1)
 
     def installSmap(self):
         #run terminal commands here
-        call(["sh", "smap_install.sh"])
-        folderName = self.entryText.get()
-        print folderName
-        call(["sh", "smap_folder.sh"])
+        #call(["sh", "smap_install.sh"])
+        #folderName = self.entryText.get()
+        #print folderName
+        #call(["sh", "smap_folder.sh"])
+        x = [i for i in range(2)]
+        for elem in x:
+            print x
+            time.sleep(1)
 
     def configureSensor(self):
-        sensorNum = self.entryText.get()
-        savedPath = os.getcwd()
-        os.chdir('./tinyos-main/apps/LightSensor')
-        call(["make", "telosb", "install,", str(sensorNum)])
-        os.chdir(savedPath)
-        print sensorNum
-        self.sensors.append("light" + str(sensorNum))
+        #sensorNum = self.entryText.get()
+        #savedPath = os.getcwd()
+        #os.chdir('./tinyos-main/apps/LightSensor')
+        #call(["make", "telosb", "install,", str(sensorNum)])
+        #os.chdir(savedPath)
+        #print sensorNum
+        #self.sensors.append("light" + str(sensorNum))
         #run terminal commands here
+        x = [i for i in range(2)]
+        for elem in x:
+            print x
+            time.sleep(1)
 
     def configureBaseStation(self):
-        savedPath = os.getcwd()
-        os.chdir('./tinyos-main/apps/BaseStation')
-        call(["make", "telosb", "install"])
-        os.chdir(savedPath)
+        #savedPath = os.getcwd()
+        #os.chdir('./tinyos-main/apps/BaseStation')
+        #call(["make", "telosb", "install"])
+        #os.chdir(savedPath)
+        x = [i for i in range(2)]
+        for elem in x:
+            print x
+            time.sleep(1)
 
 app = Application()
 app.master.title('Smart Lighting')
