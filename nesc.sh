abd17d91b86e308db8379f8aa248c67d2cd4f340
elizabethcheng <elizabeth.y.cheng@gmail.com>
2014-05-01 20:55:31 -0700

    Uncommented nesc.sh commands

--- nesc.sh
+++ nesc.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Install nesc dependencies
-#sudo apt-get install automake
-#sudo apt-get install emacs 
-#sudo apt-get install bison 
-#sudo apt-get install flex 
-#sudo apt-get install gperf 
-#sudo apt-get install git 
-#sudo apt-get install gcc 
-#sudo apt-get install java-devel 
-##sudo apt-get install gcc-c++ 
-#sudo apt-get install g++ 
-#sudo apt-get install gcc-msp430
-#
-## Installing nesc
-#git clone git://github.com/tinyos/nesc.git
-#
-#cd nesc
-#
-#./Bootstrap
-#./configure
-#make
-#sudo make install
+sudo apt-get install automake
+sudo apt-get install emacs 
+sudo apt-get install bison 
+sudo apt-get install flex 
+sudo apt-get install gperf 
+sudo apt-get install git 
+sudo apt-get install gcc 
+sudo apt-get install java-devel 
+#sudo apt-get install gcc-c++ 
+sudo apt-get install g++ 
+sudo apt-get install gcc-msp430
+
+# Installing nesc
+git clone git://github.com/tinyos/nesc.git
+
+cd nesc
+
+./Bootstrap
+./configure
+make
+sudo make install
